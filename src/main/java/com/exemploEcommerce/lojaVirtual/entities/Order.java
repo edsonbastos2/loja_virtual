b3e8b801f303b38e71b183bf14065f7154a6a306
edsonbastos2 <edsonbastos2@hotmail.com>
2026-07-13 21:05:14 -0300

    chore:adiciona configuração banco, criando entidades

---
+++ src/main/java/com/exemploEcommerce/lojaVirtual/entities/Order.java
@@ -0,0 +1,61 @@
+package com.exemploEcommerce.lojaVirtual.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private Orderstatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order(Long id, Instant moment, Orderstatus status, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Orderstatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(Orderstatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+}
---
+++ src/main/java/com/exemploEcommerce/lojaVirtual/entities/Orderstatus.java
@@ -0,0 +1,5 @@
+package com.exemploEcommerce.lojaVirtual.entities;
+
+public enum Orderstatus {
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
